49da99c74efafd251e03342ca12b95805962316d
Graham <grahamfalconer@icloud.com>
2020-06-18 11:35:58 +0100

    wrote test for app contains tester

--- tddreactgame/src/App.test.js
+++ tddreactgame/src/App.test.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import { shallow } from 'enzyme'
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-Enzyme.configure({ adapter: new Adapter() });
-
-
 import App from './App';
 
 it('renders without crashing', () => {
   shallow(<App />)
 })
+
 it('App greets the person', () => {
   const wrapper = shallow(<App />)
   const tester = <h1>Hello</h1>
